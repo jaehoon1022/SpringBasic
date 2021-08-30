f20bdb4ac28ce33eb68e6a798354abd2585c008b
jaehoon1022 <hyunsense1022@gmail.com>
2021-08-30 09:58:48 +0900

    AOP 적용

--- src/main/java/hello/hellospring/service/MemberService.java
+++ src/main/java/hello/hellospring/service/MemberService.java
@@ -6,10 +6,11 @@ import hello.hellospring.repository.MemoryMemberRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import javax.transaction.Transactional;
 import java.util.List;
 import java.util.Optional;
 
-
+@Transactional
 public class MemberService {
 
     private final MemberRepository memberRepository;
@@ -23,10 +24,10 @@ public class MemberService {
      * 회원가입
      */
     public Long join(Member member){
-        //같은 이름이 있는 중복 회원 X
-        validateDuplicateMember(member); // 중복 회원 검증
-        memberRepository.save(member);
-        return member.getId();
+            //같은 이름이 있는 중복 회원 X
+            validateDuplicateMember(member); // 중복 회원 검증
+            memberRepository.save(member);
+            return member.getId();
     }
 
     private void validateDuplicateMember(Member member) {
